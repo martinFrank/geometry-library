216514a7bcb5252c531eaa677c108ea430998084
martinFrank <martin.frank.privat@googlemail.com>
2017-10-27 09:24:58 +0200

    setup maven

--- geolib/src/test/java/de/frank/martin/geolib/PointTest.java
+++ geolib/src/test/java/de/frank/martin/geolib/PointTest.java
@@ -1,7 +1,11 @@
 package de.frank.martin.geolib;
-import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
+//import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+//import org.junit.jupiter.api.Test;
 
 import de.frank.martin.geolib.GeoPoint;
 
