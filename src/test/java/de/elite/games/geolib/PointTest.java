7e365857229e476536538ccf53cd67fbdd6bffd9
martin.frank <martin.frank.private@gmail.com>
2019-03-12 08:00:14 +0100

    simplified asserts

--- src/test/java/de/elite/games/geolib/PointTest.java
+++ src/test/java/de/elite/games/geolib/PointTest.java
@@ -2,15 +2,15 @@ package de.elite.games.geolib;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 public class PointTest {
 
 	@Test
 	public void test() {
 		GeoPoint origin = new GeoPoint();
-		GeoPoint origin2 = new GeoPoint(0,0);		
-		assertTrue(origin.equals(origin2));
+		GeoPoint origin2 = new GeoPoint(0, 0);
+		assertEquals(origin, origin2);
 	}
 
 }
