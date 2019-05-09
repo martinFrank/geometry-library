c4f31e89c44c340affb879fe63ad234a33ac5f51
martin.frank <martin.frank.private@gmail.com>
2019-05-09 11:05:40 +0200

    added mail script and failing test

---
+++ src/test/java/com/github/martinfrank/geolib/FailingTest.java
@@ -0,0 +1,12 @@
+package com.github.martinfrank.geolib;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FailingTest {
+
+    @Test
+    public void doFailTest() {
+        Assert.fail();
+    }
+}
